5bdbb9fa1255e5b772449b4cca0d932f9662c1cd
Nishant <nishantnagdeve@gmail.com>
2024-09-01 15:57:04 +0530

    updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :notes
+  #Registration for users#
   resource :users, only: [:create]
+
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
   
